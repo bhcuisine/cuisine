91644f5938bff909e6e0caa906de5dbb4b10ab2f
BH88872567 <1936068208@qq.com>
2020-09-16 09:55:02 +0800

    project framework for bhcuisine

---
+++ bhcuisine/src/main/java/com/bh/bhcuisine/BhcuisineApplication.java
@@ -0,0 +1,13 @@
+package com.bh.bhcuisine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhcuisineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhcuisineApplication.class, args);
+    }
+
+}
---
+++ bhcuisine/src/test/java/com/bh/bhcuisine/BhcuisineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bh.bhcuisine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhcuisineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
